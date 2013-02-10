6e9bb3ce43ddd3e580e1a920801760ee6bfa2c71
id-RomiOS <romios@internetdevels.net>
2013-02-10 21:48:36 +0200

    add subjects

---
+++ pages/subjects.page.php
@@ -0,0 +1,47 @@
+<?php
+
+function subjects_page_info() {
+  return array(
+    'path'    => 'subjects/%',
+    'title'   => 'subjects_page_title',
+    'content' => 'subjects_page_content',
+    'access'  => 'subjects_page_access',
+  );
+}
+
+function subjects_page_access() {
+  return TRUE;
+}
+
+function subjects_page_title($args) {
+  $group_id = $args[1];
+  return db_select_field("`groups`", "`name`", "`id` = '$group_id'");
+}
+
+function subjects_page_content($args) {
+  $group_id = $args[1];
+  $group = db_select_row("`groups`", "*", "`id` = '$group_id'");
+  $group_name = $group['name'];
+  $group_info = unserialize($group['info']);
+  $table = array();
+  $table['attributes'] = array('border' => 1);
+  $table['caption'] = $group_name;
+  $table['header'] = array(
+    array('data' => t('Subject')),
+    array('data' => t('Teacher')),
+  );
+  foreach ($group_info as $subject_id => $teachers_id) {
+    $subject_name = db_select_field("`subjects`", "`name`", "`id` = '$subject_id'");
+    $teachers_name = array();
+    foreach($teachers_id as $teacher_id) {
+      $teachers_name[] = db_select_field("`users`", "`name`", "`id` = '$teacher_id'");
+    }
+    $table['rows'][] = array(
+      array('data' => l($subject_name, 'gradebook/' . $subject_id)),
+      array('data' => implode(', ', $teachers_name)),
+    );
+  }
+  return table($table);
+}
+
+?>
